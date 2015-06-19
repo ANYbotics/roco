912818b2e92375cf671c9320100b706e300f0a44
dbellicoso <bellicosodario@gmail.com>
2015-06-19 11:15:11 +0200

    Changed method name.

--- include/roco/controllers/ControllerInterface.hpp
+++ include/roco/controllers/ControllerInterface.hpp
@@ -84,7 +84,7 @@ class ControllerInterface
   virtual bool isCheckingState() const = 0;
   virtual void setIsCheckingState(bool isChecking) = 0;
 
-  virtual const std::string getLocomotionMode() const {return "none";};
+  virtual const std::string getCurrentControlName() const {return "none";};
 };
 
 } /* namespace controllers */
